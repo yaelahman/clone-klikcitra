18e1628c89c423284e7ab7946bb123709fd28b91
Iman <yaelahman0810@gmail.com>
2022-03-05 14:12:31 +0700

    Middleware auth

--- routes/web.php
+++ routes/web.php
@@ -19,21 +19,23 @@ Route::get('/detail/{id}', 'LandingController@detail');
 Route::prefix('/admin')->group(function () {
     Auth::routes();
 
-    Route::get('/', 'HomeController@index')->name('home');
+    Route::middleware('auth')->group(function () {
+        Route::get('/', 'HomeController@index')->name('home');
 
-    Route::resource('/category', 'CategoryController');
+        Route::resource('/category', 'CategoryController');
 
-    Route::post('/product/image', 'ProductController@ImageProduct')->name('product.image');
-    Route::resource('/product', 'ProductController');
+        Route::post('/product/image', 'ProductController@ImageProduct')->name('product.image');
+        Route::resource('/product', 'ProductController');
 
-    Route::resource('/banner', 'BannerController');
+        Route::resource('/banner', 'BannerController');
 
-    Route::get('/profile', 'HomeController@profile')->name('profile.index');
-    Route::post('/profile/{profile}', 'HomeController@updateProfile')->name('profile.update');
+        Route::get('/profile', 'HomeController@profile')->name('profile.index');
+        Route::post('/profile/{profile}', 'HomeController@updateProfile')->name('profile.update');
 
-    Route::get('/settings', 'SettingsController@index')->name('settings.index');
-    Route::post('/settings', 'SettingsController@update')->name('settings.update');
+        Route::get('/settings', 'SettingsController@index')->name('settings.index');
+        Route::post('/settings', 'SettingsController@update')->name('settings.update');
 
-    Route::get('/users', 'UserController@index')->name('users.index');
-    Route::delete('/users/{id}', 'UserController@destroy')->name('users.destroy');
+        Route::get('/users', 'UserController@index')->name('users.index');
+        Route::delete('/users/{id}', 'UserController@destroy')->name('users.destroy');
+    });
 });
